1a1bb68634cb349fb2ed09db55529961f2a82612
Kevcollector <kfitz2003@gmail.com>
2024-12-28 10:45:55 +0000

    Updated to use JPA with update delete findbyemployeeid and find by department

--- src/main/java/ie/atu/week11example/PersonRepository.java
+++ src/main/java/ie/atu/week11example/PersonRepository.java
@@ -1,5 +1,7 @@
 package ie.atu.week11example;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Person findByEmployeeId(String employeeId);
+    List<Person> findByDepartment(String department);
 }
